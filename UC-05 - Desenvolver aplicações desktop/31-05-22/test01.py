6bd8724893823658364b9ede8925d2c4270b8b65
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-06-02 16:49:37 -0300

    Atv's V5-3

--- UC-05 - Desenvolver aplicações desktop/31-05-22/test01.py
+++ UC-05 - Desenvolver aplicações desktop/31-05-22/test01.py
@@ -1,8 +1,17 @@
 from tkinter import *
 
 
-def Escrever_1():
-    conta['text'] = '1'
+def Calculadora(Car):
+    if Car == '=':
+        resul['text'] = conta['text']
+        conta['text'] = ''
+    elif Car == 'CE':
+        conta['text'] = ''
+    else:
+        conta['text'] += Car
+
+
+
 
 
 
@@ -12,32 +21,28 @@ window.minsize(200, 150)
 window.maxsize(600, 550)
 window.config(bg='gray')
 
-for linha1 in range(5):
-    window.grid_rowconfigure(linha1, weight=1)
-for linha in range(4):
-    window.grid_columnconfigure(linha, weight=1)
+zero = Button(window, text='0', font='Arial 30', command=lambda: Calculadora(Car='0'))
+um = Button(window, text='1', font='Arial 30', command=lambda: Calculadora(Car='1'))
+dois = Button(window, text='2', font='Arial 30', command=lambda: Calculadora(Car='2'))
+tres = Button(window, text='3', font='Arial 30', command=lambda: Calculadora(Car='3'))
+quatro = Button(window, text='4', font='Arial 30', command=lambda: Calculadora(Car='4'))
+cinco = Button(window, text='5', font='Arial 30', command=lambda: Calculadora(Car='5'))
+seis = Button(window, text='6', font='Arial 30', command=lambda: Calculadora(Car='6'))
+sete = Button(window, text='7', font='Arial 30', command=lambda: Calculadora(Car='7'))
+oito = Button(window, text='8', font='Arial 30', command=lambda: Calculadora(Car='8'))
+nove = Button(window, text='9', font='Arial 30', command=lambda: Calculadora(Car='9'))
+botao1 = Button(window, text='+', font='Arial 30', command=lambda: Calculadora(Car='+'))
+botao2 = Button(window, text='-', font='Arial 30', command=lambda: Calculadora(Car='-'))
+botao3 = Button(window, text='x', font='Arial 30', command=lambda: Calculadora(Car='x'))
+botao4 = Button(window, text='%', font='Arial 30', command=lambda: Calculadora(Car='%'))
+botao5 = Button(window, text='=', font='Arial 30', command=lambda: Calculadora(Car='='))
+botao6 = Button(window, text='CE', font='Arial 30', command=lambda: Calculadora(Car='CE'))
+conta = Label(window, text='', font='Arial 30', bg='gray')
+resul = Label(window, text='', font='Arial 30', bg='gray')
 
-zero = Button(window, text='0', font='Arial 30')
-um = Button(window, text='1', font='Arial 30')
-dois = Button(window, text='2', font='Arial 30')
-tres = Button(window, text='3', font='Arial 30')
-quatro = Button(window, text='4', font='Arial 30')
-cinco = Button(window, text='5', font='Arial 30')
-seis = Button(window, text='6', font='Arial 30')
-sete = Button(window, text='7', font='Arial 30')
-oito = Button(window, text='8', font='Arial 30')
-nove = Button(window, text='9', font='Arial 30')
-botao1 = Button(window, text='+', font='Arial 30')
-botao2 = Button(window, text='-', font='Arial 30')
-botao3 = Button(window, text='x', font='Arial 30')
-botao4 = Button(window, text='%', font='Arial 30')
-botao5 = Button(window, text='=', font='Arial 30')
-botao6 = Button(window, text=',', font='Arial 30')
-conta = Label(window, text='conta:', font='Arial 30', bg='gray')
-resul = Label(window, text='Resultado:', font='Arial 30', bg='gray')
 
-conta.grid(row=0, column=0, sticky=NSEW)
-resul.grid(row=1, column=0, sticky=NSEW)
+conta.grid(row=0, column=1, sticky=NSEW)
+resul.grid(row=1, column=1, sticky=NSEW)
 zero.grid(row=5, column=1, sticky=NSEW)
 um.grid(row=2, column=0, sticky=NSEW)
 dois.grid(row=2, column=1, sticky=NSEW)
