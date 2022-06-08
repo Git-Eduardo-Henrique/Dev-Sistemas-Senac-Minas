4e2282ed62933af22289fc8d3405d989b06c2a01
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-06-08 16:59:53 -0300

    Atv's V8-1

--- UC-05 - Desenvolver aplicações desktop/07-06-22/Atv-01.py
+++ UC-05 - Desenvolver aplicações desktop/07-06-22/Atv-01.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 window = Tk()
-window.geometry('734x400')
+window.geometry('734x245')
 # ==========================================================================================================
 
 frame_1 = Frame(window, bg='#17A589')
@@ -16,15 +16,26 @@ frame_5 = Frame(window, highlightbackground="#17A589", highlightthickness=1)
 Text_Create = Label(frame_1, text='CREATE ACCOUNT', font='SnapITC 20', bg='#17A589', foreground='white', anchor=E, width=30)
 
 # frame 2
+Imagem_Nome = PhotoImage(file='avatar-2.png')
 Text_dados = Label(frame_2, text='Dados Pessoais', font='SnapITC 13 italic', foreground='#17A589')
 Text_Nome = Label(frame_2, text='Nome:', font='SnapITC 10 italic', foreground='#0B5345', pady=3)
 nome_Entry = Entry(frame_2, font='SnapITC 10 italic', highlightbackground="#17A589", highlightthickness=2)
+Imagem_Label_Nome = Label(frame_2, image=Imagem_Nome)
+
+Imagem_Data = PhotoImage(file='baby.png')
 Text_Data = Label(frame_2, text='Datanasc:', font='SnapITC 10 italic', foreground='#0B5345', pady=3)
 Data_Entry = Entry(frame_2, font='SnapITC 10 italic', highlightbackground="#17A589", highlightthickness=2)
+Imagem_Label_Data = Label(frame_2, image=Imagem_Data)
+
+Imagem_CPF = PhotoImage(file='carteira-de-identidade.png')
 Text_CPF = Label(frame_2, text='CPF:', font='SnapITC 10 italic', foreground='#0B5345', pady=3)
 CPF_Entry = Entry(frame_2, font='SnapITC 10 italic', highlightbackground="#17A589", highlightthickness=2)
+Imagem_Label_CPF = Label(frame_2, image=Imagem_CPF)
+
+Imagem_Telefone = PhotoImage(file='chamada-telefonica.png')
 Text_Tele = Label(frame_2, text='Telefone:', font='SnapITC 10 italic', foreground='#0B5345', pady=3)
 Tele_Entry = Entry(frame_2, font='SnapITC 10 italic', highlightbackground="#17A589", highlightthickness=2)
+Imagem_Label_Tele = Label(frame_2, image=Imagem_Telefone)
 
 # frame 3
 Text_Endereco = Label(frame_3, text='Endereço', font='SnapITC 13 italic', foreground='#17A589')
@@ -53,14 +64,18 @@ Text_Create.grid(row=0, column=0, sticky=NSEW)
 
 frame_2.grid(row=1, column=0, sticky=NSEW)
 Text_dados.grid(row=0, column=0, columnspan=2)
-Text_Nome.grid(row=1, column=0)
-nome_Entry.grid(row=1, column=1)
-Text_Data.grid(row=2, column=0)
-Data_Entry.grid(row=2, column=1)
-Text_CPF.grid(row=3, column=0)
-CPF_Entry.grid(row=3, column=1)
-Text_Tele.grid(row=4, column=0)
-Tele_Entry.grid(row=4, column=1)
+Imagem_Label_Nome.grid(row=1, column=0)
+Text_Nome.grid(row=1, column=1)
+nome_Entry.grid(row=1, column=2)
+Imagem_Label_Data.grid(row=2, column=0)
+Text_Data.grid(row=2, column=1)
+Data_Entry.grid(row=2, column=2)
+Imagem_Label_CPF.grid(row=3, column=0)
+Text_CPF.grid(row=3, column=1)
+CPF_Entry.grid(row=3, column=2)
+Imagem_Label_Tele.grid(row=4, column=0)
+Text_Tele.grid(row=4, column=1)
+Tele_Entry.grid(row=4, column=2)
 
 frame_3.grid(row=1, column=1, sticky=NSEW)
 Text_Endereco.grid(row=0, column=0, columnspan=5)
@@ -75,12 +90,12 @@ Cidade_Entry.grid(row=2, column=3, sticky=NSEW)
 Text_UF.grid(row=2, column=4, sticky=NSEW)
 UF_Entry.grid(row=2, column=5, sticky=NSEW)
 
-frame_4.grid(row=2, column=1, sticky=NSEW)
+frame_4.grid(row=2, column=0, columnspan=2, sticky=NSEW)
 Gravar.grid(row=0, column=0, sticky=NSEW)
 Limpar.grid(row=0, column=1, sticky=NSEW)
 Limpar.grid(row=0, column=3, sticky=NSEW)
 
-frame_5.grid(row=2, column=0, sticky=NSEW)
+frame_5.grid(row=3, column=0, columnspan=2, sticky=NSEW)
 Text_Alter.grid(row=0, column=0, sticky=NSEW)
 SingIN.grid(row=1, column=0)
 # ==========================================================================================================
