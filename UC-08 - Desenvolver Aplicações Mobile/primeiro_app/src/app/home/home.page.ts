f3b190e8ebd0b466027a6956e4deed4c6bfafe26
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-08-19 17:00:31 -0300

    Atv's V1.18.2

---
+++ UC-08 - Desenvolver Aplicações Mobile/primeiro_app/src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  resultado: string
+  escreva: string
+
+  constructor() {}
+  sus(){
+    this.resultado = this.escreva.toString()
+  }
+
+}
