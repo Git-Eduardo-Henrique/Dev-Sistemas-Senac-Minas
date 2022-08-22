791e3a5d40a401a1667b39fd8c2462094d76185e
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-08-22 17:08:25 -0300

    Atv's V1.18.4

--- UC-08 - Desenvolver Aplicações Mobile/primeiro_app/src/app/home/home.page.ts
+++ UC-08 - Desenvolver Aplicações Mobile/primeiro_app/src/app/home/home.page.ts
@@ -6,12 +6,46 @@ import { SubjectSubscriber } from 'rxjs/internal/Subject';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  resultado: string
-  escreva: string
+  // variaveis
+  resultado: String
+  n1: String
+  n2: String
   constructor() {}
-  escreval(){
-    this.resultado = this.escreva.toString()
+
+  // uma função em js
+  adicao(){  
+    var n1 = Number(this.n1) // variavel
+    var n2 = Number(this.n2)
+    var soma = n1 + n2
+    this.resultado = soma.toString()
   }
 
+  subtracao(){  
+    var n1 = Number(this.n1) // variavel
+    var n2 = Number(this.n2)
+    var sub = n1 - n2
+    this.resultado = sub.toString()
+  }
+
+  mult(){  
+    var n1 = Number(this.n1) // variavel
+    var n2 = Number(this.n2)
+    var multi = n1 * n2
+    this.resultado = multi.toString() 
+  }
+
+  divi(){  
+    var n1 = Number(this.n1) // variavel
+    var n2 = Number(this.n2)
+    
+    if (n2 != 0) {
+      var divi = n1 / n2
+      this.resultado = divi.toString()
+    }
+    else {
+      this.resultado = 'NÃO VAI DAAAR'.toString()
+    }
+  }
 }
