7588f4810988d33f19a53f856d9f4d12b90825e7
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-09-01 16:30:36 -0300

    Atv's V1.19.5.1

--- UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.page.ts
+++ UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.page.scss'],
 })
 export class FormularioPage implements OnInit {
+  pessoa = {}
+  pessoaForm: FormGroup
 
-  constructor() { }
+
+  constructor(private formularioBuilder: FormBuilder) { }
+
+  submit() {
+    console.log('ui')
+  }
 
   ngOnInit() {
+    this.pessoaForm = this.formularioBuilder.group({
+      nome: ['', Validators.required],
+      tele: ['', Validators.required],
+      cpf: ['', Validators.required]
+    })
   }
 
 }
