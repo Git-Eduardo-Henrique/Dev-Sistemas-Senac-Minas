c13cdbd480ac49f76951c0e38c8829012b21d243
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-09-06 17:34:04 -0300

    Atv's V1.19.6

--- UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.module.ts
+++ UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +8,16 @@ import { FormularioPageRoutingModule } from './formulario-routing.module';
 
 import { FormularioPage } from './formulario.page';
 
+import { SimpleMaskModule } from 'ngx-ion-simple-mask'
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FormularioPageRoutingModule
+    FormularioPageRoutingModule,
+    ReactiveFormsModule,
+    SimpleMaskModule
   ],
   declarations: [FormularioPage]
 })
--- UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.page.ts
+++ UC-08 - Desenvolver Aplicações Mobile/formulario/src/app/formulario/formulario.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-formulario',
@@ -11,17 +12,32 @@ export class FormularioPage implements OnInit {
   pessoaForm: FormGroup
 
 
-  constructor(private formularioBuilder: FormBuilder) { }
+  constructor(private formularioBuilder: FormBuilder, private alertController: AlertController) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Dados Invalidos',
+      subHeader: 'Siga a orientação certa ',
+      buttons: ['fechar'],
+    })
+    await alert.present()
+  }
 
   submit() {
-    console.log('ui')
+    if (this.pessoaForm.valid){
+      console.log(this.pessoa)
+    }
+    else {
+      this.presentAlert()
+    }
   }
 
   ngOnInit() {
     this.pessoaForm = this.formularioBuilder.group({
-      nome: ['', Validators.required],
-      tele: ['', Validators.required],
-      cpf: ['', Validators.required]
+      nome: ['', Validators.compose([Validators.required, Validators.maxLength(45), Validators.minLength(3)])],
+      email: ['', Validators.compose([Validators.required, Validators.maxLength(45), Validators.email])],
+      tele: ['', Validators.compose([Validators.required, Validators.maxLength(11), Validators.minLength(11)])],
+      cpf: ['', Validators.compose([Validators.required, Validators.minLength(14)])]
     })
   }
 
